be97277eda8a65b8436910acad30029da47f559d
Vitaliy <vitstar@gmail.com>
2019-06-17 18:31:56 +0600

    new file:   src/main/java/com/example/sweater/config/MailConfig.java 	new file:   src/main/java/com/example/sweater/controller/UserController.java 	new file:   src/main/java/com/example/sweater/service/MailSender.java 	modified:   src/main/java/com/example/sweater/service/UserSevice.java 	renamed:    src/main/resources/templates/login.mustache -> src/main/resources/static/style.css 	new file:   src/main/resources/templates/greeting.ftl 	deleted:    src/main/resources/templates/greeting.mustache 	new file:   src/main/resources/templates/main.ftl 	deleted:    src/main/resources/templates/main.mustache 	new file:   src/main/resources/templates/parts/common.ftl 	new file:   src/main/resources/templates/parts/login.ftl 	new file:   src/main/resources/templates/registration.ftl 	deleted:    src/main/resources/templates/registration.mustache 	new file:   src/main/resources/templates/userEdit.ftl 	new file:   src/main/resources/templates/userList.ftl 	modified:   .gitignore 	modified:   pom.xml 	modified:   src/main/java/com/example/sweater/config/MailConfig.java 	modified:   src/main/java/com/example/sweater/config/MvcConfig.java 	modified:   src/main/java/com/example/sweater/config/WebSecurityConfig.java 	modified:   src/main/java/com/example/sweater/controller/MainController.java 	modified:   src/main/java/com/example/sweater/controller/RegistrationController.java 	modified:   src/main/java/com/example/sweater/domain/Message.java 	modified:   src/main/java/com/example/sweater/domain/Role.java 	modified:   src/main/java/com/example/sweater/domain/User.java 	modified:   src/main/java/com/example/sweater/repos/UserRepo.java 	modified:   src/main/java/com/example/sweater/service/MailSender.java 	modified:   src/main/java/com/example/sweater/service/UserSevice.java 	modified:   src/main/resources/application.properties 	modified:   src/main/resources/static/style.css modified:   src/main/resources/templates/greeting.ftl 	modified:   src/main/resources/templates/main.ftl 	modified:   src/main/resources/templates/parts/common.ftl 	modified:   src/main/resources/templates/parts/login.ftl 	modified:   src/main/resources/templates/registration.ftl 	modified:   sweater.iml

--- src/main/java/com/example/sweater/service/UserSevice.java
+++ src/main/java/com/example/sweater/service/UserSevice.java
@@ -1,5 +1,7 @@
 package com.example.sweater.service;
 
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,13 +9,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
-public class UserSevice implements UserDetailsService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+       if (userFromDb !=null){
+           return false;
+       }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+        return  true;
     }
 }
